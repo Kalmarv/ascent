1959c71176eca52a1c0c9abe27f3456af533bc9d
Kalmarv <59708937+Kalmarv@users.noreply.github.com>
2022-05-29 16:59:56 -0700

    :recycle: Lower distance for color extraction for images that need it

--- lib/getColors.ts
+++ lib/getColors.ts
@@ -7,10 +7,10 @@ import { Colors } from '../types/types'
 export const getColors = async (imageURL: string): Promise<Colors[]> => {
   const colorRes = await extractColors(imageURL, { crossOrigin: 'Anonymous' })
 
-  // if (colorRes.length === 1) {
-  //   const finalColors = await extractColors(imageURL, { crossOrigin: 'Anonymous', distance: 0 })
-  //   return finalColors
-  // } else {
-  return colorRes
-  // }
+  if (colorRes.length === 1) {
+    const finalColors = await extractColors(imageURL, { crossOrigin: 'Anonymous', distance: 0 })
+    return finalColors
+  } else {
+    return colorRes
+  }
 }
