050eb100f4e2c4b66478b23267c7e5104d705eb2
Kalmarv <59708937+Kalmarv@users.noreply.github.com>
2022-06-07 19:36:52 -0600

    ♻️ Change lighting

--- src/scene/scene.tsx
+++ src/scene/scene.tsx
@@ -1,5 +1,5 @@
 import { Canvas } from '@react-three/fiber'
-import { Environment, OrbitControls } from '@react-three/drei'
+import { OrbitControls } from '@react-three/drei'
 import { Colors, lastFmSongProps } from '../types/types'
 import Album from './album'
 import { useEffect, useMemo, useState } from 'react'
@@ -27,12 +27,11 @@ const Scene: React.FC<lastFmSongProps> = ({ song }) => {
         // background: `linear-gradient(to bottom right, ${songColors[0]?.hex}, ${songColors[1]?.hex})`,
       }}
     >
-      <ambientLight intensity={0.5} />
-      <pointLight position={[-10, -10, -10]} />
+      {/* Tried to match the brightness to what I see visually in spotify */}
+      <ambientLight intensity={0.8} />
       <Album cover={song.image} scale={[1, 1, 0.05]} />
       <Background colors={songColors} />
       <OrbitControls />
-      <Environment preset={'dawn'} />
     </Canvas>
   )
 }
