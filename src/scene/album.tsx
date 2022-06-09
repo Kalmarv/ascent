15322c83f2cd1bfece0d549ef8cd900cd42ee031
Kalmarv <59708937+Kalmarv@users.noreply.github.com>
2022-06-08 20:30:24 -0600

    ✨ Make album scale on hover

--- src/scene/album.tsx
+++ src/scene/album.tsx
@@ -1,24 +1,44 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { TextureLoader } from 'three'
 import { useLoader } from '@react-three/fiber'
 import { AlbumProps } from '../types/types'
+import { useSpring, animated, config } from '@react-spring/three'
 
 const Album = ({ cover, ...props }: AlbumProps) => {
   const mesh = useRef<THREE.Mesh>(null!)
   const albumCover = useLoader(TextureLoader, cover)
+  const [hovered, hover] = useState(false)
+
+  const { scale } = useSpring({
+    scale: hovered ? 1.25 : 1,
+    config: config.wobbly,
+  })
 
   // For the life of me I couldn't figure out how to use different
   // materials for different faces of the cube.
   return (
     <>
-      <mesh ref={mesh} scale={[1, 1, 1]}>
+      <animated.mesh
+        ref={mesh}
+        scale={scale}
+        onPointerOver={(event) => hover(true)}
+        onPointerOut={(event) => hover(false)}
+      >
         <planeBufferGeometry />
         <meshStandardMaterial map={albumCover} />
-      </mesh>
-      <mesh position={[0, 0, -0.014]} scale={[1, 1, 0.025]}>
+      </animated.mesh>
+      <animated.mesh
+        position={[0, 0, -0.014]}
+        onPointerOver={(event) => hover(true)}
+        onPointerOut={(event) => hover(false)}
+        // have to separate the axis to keep reactive spring values as well as fixed z-scale
+        scale-x={scale}
+        scale-y={scale}
+        scale-z={0.025}
+      >
         <boxBufferGeometry />
         <meshBasicMaterial color="#121212" />
-      </mesh>
+      </animated.mesh>
     </>
   )
 }
